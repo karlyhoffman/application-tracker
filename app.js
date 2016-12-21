b598878882f604d3ef046271780ef58c9691c207
Karly <Karly@HungryHoffKitchen.local>
2016-12-21 15:56:31 -0800

    CRUD functionality complete. Next steps: build Angular files.

--- app.js
+++ app.js
@@ -17,20 +17,14 @@ app.set('view engine', 'hbs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+    res.render('angular');  
+});
+
 // controllers
-app.use('/', tracker); 
-
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
+app.use('/jobs', tracker); 
 
 app.listen(3000, function(){
 	console.log("The server is running. Catch it!");
--- controllers/tracker.js
+++ controllers/tracker.js
@@ -1,54 +1,44 @@
 const express 	= require('express');
-const trackerCtrl = express.Router();
+const controller = express.Router();
 
 const jobModel = require('../models/Job');
 const bodyParser = require('body-parser');
 
 
-trackerCtrl.get('/', function(req, res){
-	res.render('angular')
-});
-
-// trackerCtrl.post('/', function(req, res){
-// 	res.send('POST route on tracker.');
-// });
-
-
-
 // RESTful API
 
-// //get all
-// trackerCtrl.get('/', function(req, res, next) {
-//   jobModel.find(function(error, jobs) {
-//     if (error) return error;
-//     res.json(jobs);
-//   });
-// });
+//get all
+controller.get('/', function(req, res, next) {
+  jobModel.find(function(error, jobs) {
+    if (error) return error;
+    res.json(jobs);
+  });
+});
 
-// //get by id
-// trackerCtrl.get('/:id', function(req, res, next) {
-//   jobModel.findById(req.params.id, function(error, job) {
-//     if (error) return error;
-//     res.json(job);
-//   });
-// });
+//get by id
+controller.get('/:id', function(req, res, next) {
+  jobModel.findById(req.params.id, function(error, job) {
+    if (error) return error;
+    res.json(job);
+  });
+});
 
-// //create
-// trackerCtrl.post('/', function(req, res, next) {
-//   jobModel.create(req.body, function(error, job) {
-//     if (error) return error;
-//     res.json(job);
-//   });
-// });
+//create
+controller.post('/', function(req, res, next) {
+  jobModel.create(req.body, function(error, job) {
+    if (error) return error;
+    res.json(job);
+  });
+});
 
 //update by id
-trackerCtrl.put('/:id', function(req, res, next) {
+controller.put('/:id', function(req, res, next) {
   jobModel.findByIdAndUpdate(req.params.id, req.body, function(error, job) {
     if (error) return error;
     res.json(job);
   })
 });
-trackerCtrl.patch('/:id', function(req, res, next) {
+controller.patch('/:id', function(req, res, next) {
   jobModel.findByIdAndUpdate(req.params.id, req.body, function(error, job) {
     if (error) return error;
     res.json(job);
@@ -56,14 +46,13 @@ trackerCtrl.patch('/:id', function(req, res, next) {
 });
 
 //delete by id
-trackerCtrl.delete('/:id', function(req, res, next) {
+controller.delete('/:id', function(req, res, next) {
   jobModel.findByIdAndRemove(req.params.id, req.body, function(error, job) {
     if (error) return error;
     res.json({
-      "Message": "User with the id of " + job.id + " has been removed"
+      "Message": "Job with the title of " + job.jobTitle + " has been removed"
     });
   });
 });
 
-
-module.exports = trackerCtrl;
+module.exports = controller;
