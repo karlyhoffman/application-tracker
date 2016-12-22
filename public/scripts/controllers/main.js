a1650fdb434cab23ada16b8bc03ce9318d660a2b
Karly Hoffman <karlyhoffman@gmail.com>
2016-12-22 14:06:54 -0800

    Completed: 'add' route and post request working. Next Step: add patch request on home view.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,11 +1,11 @@
 angular.module('jobTrackerApp')
-  .controller('mainCtrl', function($scope, $http, dataService) {
+  .controller('mainCtrl', function($scope, $http, $location, dataService) {
 
     dataService.getJobs(function(res) {
       $scope.jobs = res.data;
     });
 
+    $scope.addPage = function() {
+		$location.path('/new');
+	}
 });
-
-
-
--- public/scripts/controllers/new.js
+++ public/scripts/controllers/new.js
@@ -1,5 +1,24 @@
 angular.module('jobTrackerApp')
-  .controller('addCtrl', function($scope) {
+  .controller('addCtrl', function($scope, $location, $http) {
 
+  	$scope.addJobApp = function(submitDate, jobTitle, company, jobDescLink, interviewerNameAndTitle, interviewerContact, interviewerNotes, followUpEmail) {
+  		 $http({
+	        url: 'http://localhost:3000/jobs',
+	        method: "POST",
+	        data: { submitDate: submitDate, jobTitle: jobTitle, company: company, jobDescLink: jobDescLink, interviewerNameAndTitle: interviewerNameAndTitle, interviewerContact: interviewerContact, interviewerNotes: interviewerNotes, followUpEmail: followUpEmail} 
+	    })
+	    .then(function(response) {
+	    	console.log(response);
+	    	$scope.homePage();
+	    }, 
+	    function(response) {
+	    	console.log(response);
+	    });
+	};
+
+  	$scope.homePage = function() {
+		$location.path('/');
+	};
 
 });
+
