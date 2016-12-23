f647fabc740f918c14447e5dff69474a8704c9c4
Karly Hoffman <karlyhoffman@gmail.com>
2016-12-23 09:19:51 -0800

    Coverted json date to readable time

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 angular.module('jobTrackerApp')
-  .controller('mainCtrl', function($scope, $http, $location, dataService) {
+ .controller('mainCtrl', function($scope, $http, $location, dataService) {
 
   	$scope.jobs = [];
 
     dataService.getJobs(function(res) {
-      $scope.jobs = res.data;
+    	$scope.jobs = res.data;
+		for (var i = 0; i < $scope.jobs.length; i++) {
+			// console.log(moment($scope.jobs[i].submitDate).format('MM-DD-YYYY'));
+			// *Format Options: http://momentjs.com/
+			$scope.jobs[i].submitDate = moment($scope.jobs[i].submitDate).format('dddd, MMM Do');
+		}
     });
 
     $scope.addPage = function() {
